aa08b5039654d7b2e1e447763999630a60e13cfe
tonousa <tonousa@hotmail.com>
2015-12-07 18:48:48 -0500

    2

---
+++ MvcMusicStore2/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        //public ActionResult Index()
+        public String Index()
+        {
+            //return View();
+            return "Hello from Store.Index()";
+        }
+
+        public String Browse(string genre)
+        {
+            //return View();
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message;
+        }
+
+        public String Details(int id)
+        {
+            //return View();
+            string message = HttpUtility.HtmlEncode("Store.Details, id = " + id);
+            return message;
+        }
+
+    }
+}
