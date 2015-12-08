021e05606528fdec58000ceb806fe457f646d8ab
tonousa <tonousa@hotmail.com>
2015-12-08 10:52:06 -0500

    Scafolding

---
+++ MvcMusicStore2/Models/MusicStoreDB.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore2.Models
+{
+    public class MusicStoreDB : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<MvcMusicStore2.Models.MusicStoreDB>());
+
+        public MusicStoreDB() : base("name=MusicStoreDB")
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
